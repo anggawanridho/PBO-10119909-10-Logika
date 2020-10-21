4e00c4faf0b43251e41d90328b1273f72a730532
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-21 10:52:32 +0700

    Add files via upload
    first commit

---
+++ src/latihan10logika/Latihan10Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan10logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur  = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "+ dewasa);
+    }
+    
+}
